0af5a5d1d6a827c58f57bdc877e63ef18170dc85
TS00001 <info@ts-promotion.de>
2023-05-09 15:23:33 +0200

    init

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,11 @@ fun main(){
 
     var nikeSchuh = Schuh("Nike Air Jordan", 120.00, "★★★★★", 40)
 
-    nikeSchuh.groesseWaehlen()
+    val user1 = User()
+    user1.register()
+    user1.aufladen(300.0)
+    user1.showUsers()
+
 
 
 }
--- src/main/kotlin/User.kt
+++ src/main/kotlin/User.kt
@@ -1,14 +1,23 @@
 import java.util.*
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 class User {
     var username: String = ""
     var email: String = ""
     var password: String = ""
+    var geburtsdatum: LocalDate? = null
+    var guthaben:  Double = 0.0
 
     companion object {
         val users = mutableListOf<User>()
     }
 
-    fun register() {
+    fun aufladen(amount: Double) {
+        guthaben += amount
+        println("✓ Guthaben erfolgreich um $amount Euro aufgeladen. Neues Guthaben: $guthaben Euro")
+    }
+
+    fun register(guthaben: Double = 0.0) {
         val scanner = Scanner(System.`in`)
 
         print("Bitte geben Sie Ihren Benutzernamen ein: ")
@@ -20,10 +29,24 @@ class User {
         print("Bitte geben Sie Ihr Passwort ein: ")
         password = scanner.nextLine()
 
+        print("Bitte geben Sie Ihr Geburtsdatum ein (Format: dd.MM.yyyy): ")
+        val birthDateStr = scanner.nextLine()
+        geburtsdatum = try {
+            LocalDate.parse(birthDateStr, DateTimeFormatter.ofPattern("dd.MM.yyyy"))
+        } catch (e: Exception) {
+            null
+        }
+
         if (users.any { it.email == email }) {
             println("✗ Die E-Mail-Adresse ist bereits registriert.")
             return
         }
+        if (geburtsdatum == null || LocalDate.now().minusYears(13).isBefore(geburtsdatum)) {
+            println("✗ Sie müssen mindestens 13 Jahre alt sein, um sich zu registrieren.")
+            return
+        }
+
+
         users.add(this)
         println("✓ Erfolgreich registriert.")
     }
@@ -32,12 +55,14 @@ class User {
         if (users.isNotEmpty()) {
             println("Liste aller registrierten Benutzer:")
             for (user in users) {
-                println("👤 Benutzername: ${user.username}, E-Mail: ${user.email}")
+                println("👤 Benutzername: ${user.username}, E-Mail: ${user.email}, Guthaben:${user.guthaben}€")
             }
         } else {
             println("Es sind keine Benutzer registriert.")
         }
 
     }
+
+
 }
 
